6ddb931bafbf7207fc945797f3808c2cde3d742f
mark <mark.poling@gmail.com>
2019-12-04 22:51:37 -0500

    housekeeping. moving folders around.

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/gojou/serverpractice/pkg/servers"
+	"serverpractice/pkg/servers"
 )
 
 func main() {
--- pkg/servers/server.go
+++ pkg/servers/server.go
@@ -1,5 +1,8 @@
 package server
 
+import "context"
+
+//Server interface
 type Server interface {
 	Get(ctx context.Context, id int64) (interface{}, err)
 }
